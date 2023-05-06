d01bb153c75fba84bd566c87eacf1e1ac646cfd2
FatlindBytyqi <95626338+FatlindBytyqi@users.noreply.github.com>
2023-05-06 18:19:53 +0200

    Added DbSets

--- smis/SchoolManagmentSystem/Data/ApplicationDbContext.cs
+++ smis/SchoolManagmentSystem/Data/ApplicationDbContext.cs
@@ -13,6 +13,14 @@ namespace SchoolManagmentSystem.Data
         }
         public DbSet<Course> Courses { get; set; }
         public DbSet<Student> Students { get; set; }
+        public DbSet<DeptBranch> DeptBranches { get; set; }
+        public DbSet<CourseAssignment> CourseAssignments { get; set; }
+        public DbSet<AcStaff> AcStaffs { get; set; }
+        public DbSet<Assistant> Assistants { get; set; }
+        public DbSet<Branch> Branches { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Professor> Professors { get; set; }
 
 
     }
