e1dba706aca683218ee41043935707923193c647
tringahaliti <th53768@ubt-uni.net>
2023-05-06 15:31:25 +0200

    Added Department Model

---
+++ smis/SchoolManagmentSystem/Models/Department.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SchoolManagmentSystem.Models
+{
+	public class Department
+	{
+		[Key]
+		public int DepartmentID { get; set; }
+
+        [Required]
+        [StringLength(25, ErrorMessage = "Name cannot be longer than 25 characters")]
+        public string Name { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime CreatedDate { get; set; }
+
+        //relationships
+
+
+    }
+}
+
