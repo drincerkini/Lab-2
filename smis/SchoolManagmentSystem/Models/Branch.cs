f086eda15f2af153538aa1610faf67e55780392f
FatlindBytyqi <95626338+FatlindBytyqi@users.noreply.github.com>
2023-05-06 16:46:00 +0200

    Fixed Branch Data Validation

--- smis/SchoolManagmentSystem/Models/Branch.cs
+++ smis/SchoolManagmentSystem/Models/Branch.cs
@@ -3,33 +3,33 @@ using System.ComponentModel.DataAnnotations;
 
 namespace SchoolManagmentSystem.Models
 {
-	public enum Location
-	{
-		Prishtinë,
-		Ferizaj,
-		Gjilan,
-		Pejë,
-		Lipjan,
-		Prizren
-	}
+    public enum Location
+    {
+        Prishtinë,
+        Ferizaj,
+        Gjilan,
+        Pejë,
+        Lipjan,
+        Prizren
+    }
 
-	public class Branch
-	{
-		[Key]
-		public int BranchID { get; set; }
-
-		[Required]
-		public int SMIAL { get; set; }
+    public class Branch
+    {
+        [Key]
+        public int BranchID { get; set; }
 
         [Required]
-        [StringLength(15, ErrorMessage = "Name cannot be longer than 15 characters")]
+        public int SMIAL { get; set; }
+
+        [Required]
+        [StringLength(20, MinimumLength = 2, ErrorMessage = "Name cannot be shorter than 2 characters or longer than 20 characters.")]
         public string Name { get; set; }
 
-		public Location? Location { get; set; }
+        public Location? Location { get; set; }
 
 
-		//relationship
+        //relationship
 
-	}
+    }
 }
 
