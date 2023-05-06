34ab2f503e226117e36d1afd100c05f2a2e9c999
drincerkini <93202832+drincerkini@users.noreply.github.com>
2023-05-06 22:12:47 +0200

    Created Professor Scafolded Item

---
+++ smis/SchoolManagmentSystem/Controllers/ProfessorsController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SchoolManagmentSystem.Data;
+using SchoolManagmentSystem.Models;
+
+namespace SchoolManagmentSystem.Controllers
+{
+    public class ProfessorsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProfessorsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Professors
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Professors.Include(p => p.Department);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Professors/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Professors == null)
+            {
+                return NotFound();
+            }
+
+            var professor = await _context.Professors
+                .Include(p => p.Department)
+                .FirstOrDefaultAsync(m => m.ProfessorID == id);
+            if (professor == null)
+            {
+                return NotFound();
+            }
+
+            return View(professor);
+        }
+
+        // GET: Professors/Create
+        public IActionResult Create()
+        {
+            ViewData["DepartmentID"] = new SelectList(_context.Departments, "DepartmentID", "Name");
+            return View();
+        }
+
+        // POST: Professors/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ProfessorID,Name,Surname,Email,BirthDate,HireDate,Address,DepartmentID")] Professor professor)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(professor);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["DepartmentID"] = new SelectList(_context.Departments, "DepartmentID", "Name", professor.DepartmentID);
+            return View(professor);
+        }
+
+        // GET: Professors/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Professors == null)
+            {
+                return NotFound();
+            }
+
+            var professor = await _context.Professors.FindAsync(id);
+            if (professor == null)
+            {
+                return NotFound();
+            }
+            ViewData["DepartmentID"] = new SelectList(_context.Departments, "DepartmentID", "Name", professor.DepartmentID);
+            return View(professor);
+        }
+
+        // POST: Professors/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ProfessorID,Name,Surname,Email,BirthDate,HireDate,Address,DepartmentID")] Professor professor)
+        {
+            if (id != professor.ProfessorID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(professor);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProfessorExists(professor.ProfessorID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["DepartmentID"] = new SelectList(_context.Departments, "DepartmentID", "Name", professor.DepartmentID);
+            return View(professor);
+        }
+
+        // GET: Professors/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Professors == null)
+            {
+                return NotFound();
+            }
+
+            var professor = await _context.Professors
+                .Include(p => p.Department)
+                .FirstOrDefaultAsync(m => m.ProfessorID == id);
+            if (professor == null)
+            {
+                return NotFound();
+            }
+
+            return View(professor);
+        }
+
+        // POST: Professors/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Professors == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Professors'  is null.");
+            }
+            var professor = await _context.Professors.FindAsync(id);
+            if (professor != null)
+            {
+                _context.Professors.Remove(professor);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProfessorExists(int id)
+        {
+          return _context.Professors.Any(e => e.ProfessorID == id);
+        }
+    }
+}
