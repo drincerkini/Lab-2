a57eebf0f563878da88186299403114651704d49
FatlindBytyqi <95626338+FatlindBytyqi@users.noreply.github.com>
2023-05-06 15:55:12 +0200

    Created Course Model

---
+++ smis/SchoolManagmentSystem/Models/Course.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SchoolManagmentSystem.Models
+{
+    public class Course
+    {
+        [Key]
+        [Display(Name = "Number")]
+        public int CourseID { get; set; }
+        [Required]
+        [StringLength(25, MinimumLength = 3)]
+        public string? Title { get; set; }
+        [Required]
+        [Range(0, 12)]
+        public int ECTS { get; set; }
+
+
+    }
+}
