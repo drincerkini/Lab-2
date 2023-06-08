a82bfffae141d8b19c1011da8264285d55f46618
tringahaliti <th53768@ubt-uni.net>
2023-06-08 19:03:24 +0200

    Created Departement form page

---
+++ ubt-web/clientapp/src/pages/CreateBranchPage.vue
@@ -0,0 +1,17 @@
+<template> 
+
+<CreateBranchComponent />
+
+</template>
+
+
+<script>
+import CreateBranchComponent from '../components/CreateBranchComponent'
+
+export default {
+    components: {
+        CreateBranchComponent
+    }
+}
+
+</script>
