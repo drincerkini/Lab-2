b770f5b52a0ea96f75e015b1d58bea8173e163e7
tringahaliti <th53768@ubt-uni.net>
2023-06-08 19:03:01 +0200

    Created Page for Departments

---
+++ ubt-web/clientapp/src/pages/BranchPage.vue
@@ -0,0 +1,17 @@
+<template> 
+
+<BranchCardComponent />
+
+</template>
+
+
+<script>
+import BranchCardComponent from '../components/BranchCardComponent'
+
+export default {
+    components: {
+        BranchCardComponent
+    }
+}
+
+</script>
