c5946ca1ca7d91b4f442b7b4feb1f88408f8fa64
tringahaliti <th53768@ubt-uni.net>
2023-06-08 17:37:46 +0200

    created department pages

---
+++ ubt-web/clientapp/src/pages/CreateDepartmentPage.vue
@@ -0,0 +1,17 @@
+<template> 
+
+<CreateDepartmentComponent />
+
+</template>
+
+
+<script>
+import CreateDepartmentComponent from '../components/CreateDepartmentComponent'
+
+export default {
+    components: {
+        CreateDepartmentComponent
+    }
+}
+
+</script>
---
+++ ubt-web/clientapp/src/pages/DepartmentPage.vue
@@ -0,0 +1,17 @@
+<template> 
+
+<DepartmentCardComponent />
+
+</template>
+
+
+<script>
+import DepartmentCardComponent from '../components/DepartmentCardComponent'
+
+export default {
+    components: {
+        DepartmentCardComponent
+    }
+}
+
+</script>
