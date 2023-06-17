d271dee5483c0daff3cb3f6cca5a2feeae294162
tringahaliti <th53768@ubt-uni.net>
2023-06-17 16:22:59 +0200

    Added contact table for listing contacts of users

---
+++ ubt-web/clientapp/src/pages/ContactListPage.vue
@@ -0,0 +1,17 @@
+<template> 
+
+<ContactTableComponent />
+
+</template>
+
+
+<script>
+import ContactTableComponent from '../components/ContactTableComponent'
+
+export default {
+    components: {
+        ContactTableComponent
+    }
+}
+
+</script>
